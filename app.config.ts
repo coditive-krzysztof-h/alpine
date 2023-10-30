da7e3c4f1f2b5bac8ab5fe99defaa547e628e4ec
Krzysztof Hęciek <57355387+coditive-krzysztof-h@users.noreply.github.com>
2023-10-30 15:06:31 +0100

    Change theme

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Alp',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
@@ -9,7 +9,7 @@ export default defineAppConfig({
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
@@ -22,7 +22,7 @@ export default defineAppConfig({
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
       navigation: true, // possible value are : true | false
-      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
+      alignment: 'right', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
@@ -36,6 +36,12 @@ export default defineAppConfig({
     },
     form: {
       successMessage: 'Message sent. Thank you!'
+    }
+  },
+
+  prose: {
+    headings: {
+      icon: 'material-symbols:bookmark-outline'
     }
   }
 })
